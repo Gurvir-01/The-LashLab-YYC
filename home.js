15b5a6f16d5ff19b40d93c6becb4a8d04b86dbe3
Gurvir-01 <gurvir-k@hotmail.com>
2022-05-31 17:04:41 -0600

    tweaked slider pics, title, and created faq section

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
 // js maybe some react or angular where it can be applied. replace 
 //find something thick and heavy for the fonts 
 // google analytics 
+// mobile version
+// api for reviews
+// Serach engine optimization
