f63b43fd674c53113a1156baad61edc0cacd9182
Gurvir-01 <gurvir-k@hotmail.com>
2025-04-06 19:52:41 -0600

    Final Changes for css and some html changes done

--- home.js
+++ home.js
@@ -1,6 +1,23 @@
-// js maybe some react or angular where it can be applied. replace 
-//find something thick and heavy for the fonts 
-// google analytics 
+// js maybe some react or angular where it can be applied(the faq section). replace 
+// find something thick and heavy for the fonts 
 // mobile version
 // api for reviews
 // Serach engine optimization
+
+//
+
+
+//finish the faq section
+//make the card flippers work and look nice
+//correct spacing between sections
+//do a nice animation for links 
+// google analytics 
+//find good font(thick and heavy)
+// SEO
+
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const faqItem = question.parentElement;
+        faqItem.classList.toggle('open');
+    });
+});
---
+++ practice.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverse;
+        for(int i = String.valueof(x).length; i > 0; i--){
+            reverse = x
+        }
+    }
+}
